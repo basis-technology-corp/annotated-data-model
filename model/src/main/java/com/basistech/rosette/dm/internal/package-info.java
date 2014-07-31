774abac8f52a9446408724bbde33c961a5b41e06
Joel Barry <joelb@basistech.com>
2014-07-31 10:49:11 -0400

    COMN-62: Forgot this file

---
+++ model/src/main/java/com/basistech/rosette/dm/internal/package-info.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ ** This data and information is proprietary to, and a valuable trade secret
+ ** of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ ** and may only be used as permitted under the license agreement under which
+ ** it has been distributed, and in no other way.
+ **
+ ** Copyright (c) 2014 Basis Technology Corporation All rights reserved.
+ **
+ ** The technical data and information provided herein are provided with
+ ** `limited rights', and the computer software provided herein is provided
+ ** with `restricted rights' as those terms are defined in DAR and ASPR
+ ** 7-104.9(a).
+ ******************************************************************************/
+
+/**
+ * Internal classes.
+ *
+ * This package contains internal, though public, classes that are not meant to
+ * be used by clients of the ADM.  Use at your peril - they may change at will.
+ */
+package com.basistech.rosette.dm.internal;
