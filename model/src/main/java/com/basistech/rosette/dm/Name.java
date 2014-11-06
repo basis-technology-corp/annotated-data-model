820aee97e11c3d49774413e96790522f78610cb8
Benson Margulies <benson@basistech.com>
2014-11-06 11:05:24 -0500

    COMN-137: initial trivia.

---
+++ model/src/main/java/com/basistech/rosette/dm/Name.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ ** This data and information is proprietary to, and a valuable trade secret
+ ** of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ ** and may only be used as permitted under the license agreement under which
+ ** it has been distributed, and in no other way.
+ **
+ ** Copyright (c) 2014 Basis Technology Corporation All rights reserved.
+ **
+ ** The technical data and information provided herein are provided with
+ ** `limited rights', and the computer software provided herein is provided
+ ** with `restricted rights' as those terms are defined in DAR and ASPR
+ ** 7-104.9(a).
+ ******************************************************************************/
+
+package com.basistech.rosette.dm;
+
+/**
+ * A name of something in the world.
+ * Both Names and {@link com.basistech.rosette.dm.EntityMention} objects can contains the names of things.
+ * {@linkplain com.basistech.rosette.dm.EntityMention} is used for reference inside of documents, while
+ * {@linkplain Name} is used for names unrelated to documents.
+ */
+public class Name {
+    private final String text;
+    
+}
