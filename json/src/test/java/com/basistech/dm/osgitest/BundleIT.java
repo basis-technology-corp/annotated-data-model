8375586eb1bd5ee5dce0c04c057cc30d9dcb4c5c
Benson Margulies <benson@basistech.com>
2015-08-12 11:48:07 -0400

    ROS-87: just one json dir.

--- json/src/test/java/com/basistech/dm/osgitest/BundleIT.java
+++ json/src/test/java/com/basistech/dm/osgitest/BundleIT.java
@@ -14,59 +14,43 @@
 
 package com.basistech.dm.osgitest;
 
-import com.basistech.rosette.dm.jackson.AnnotatedDataModelModule;
+import com.basistech.osgi.testsupport.OsgiTestUtil;
+import com.basistech.osgi.testsupport.paxfixes.LoggingContainerFactory;
 import com.basistech.rosette.dm.LanguageDetection;
 import com.basistech.rosette.dm.Name;
+import com.basistech.rosette.dm.jackson.AnnotatedDataModelModule;
 import com.basistech.util.ISO15924;
 import com.basistech.util.LanguageCode;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
-import com.google.common.io.Resources;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.ExamFactory;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.Properties;
-
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.provision;
-import static org.ops4j.pax.exam.CoreOptions.systemPackages;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 /**
  * IT to show that the OSGi bundle works somewhat.
  */
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
+@ExamFactory(LoggingContainerFactory.class)
 public class BundleIT {
 
     private ObjectMapper objectMapper() {
         return AnnotatedDataModelModule.setupObjectMapper(new ObjectMapper());
-    }
-
-    private String getDependencyVersion(String groupId, String artifactId) {
-        URL depPropsUrl = Resources.getResource("META-INF/maven/dependencies.properties");
-        Properties depProps = new Properties();
-        try {
-            depProps.load(Resources.asByteSource(depPropsUrl).openStream());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return (String)depProps.get(String.format("%s/%s/version", groupId, artifactId));
     }
 
     @Configuration
@@ -74,37 +58,10 @@ public class BundleIT {
         String projectBuildDirectory = System.getProperty("project.build.directory");
         String projectVersion = System.getProperty("project.version");
 
-        List<String> bundleUrls = Lists.newArrayList();
-        File bundleDir = new File(projectBuildDirectory, "bundles");
-        File[] bundleFiles = bundleDir.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".jar");
-            }
-        });
-        for (File bundleFile : bundleFiles) {
-            try {
-                bundleUrls.add(bundleFile.toURI().toURL().toExternalForm());
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        bundleUrls.add(String.format("file:%s/adm-json-osgi-%s.jar", projectBuildDirectory, projectVersion));
-
-        String[] bundles = bundleUrls.toArray(new String[bundleUrls.size()]);
-        return options(
-                provision(bundles),
-                systemPackages(
-                        // These are needed for guava.
-                        "sun.misc",
-                        "javax.annotation",
-                        String.format("org.slf4j;version=\"%s\"", getDependencyVersion("org.slf4j", "slf4j-api"))
-
-                ),
-                junitBundles(),
-                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN")
-        );
+        return OsgiTestUtil.standardPaxExamConfig(new File(projectBuildDirectory),
+                projectVersion,
+                "adm-json",
+                false);
     }
 
     @Test
