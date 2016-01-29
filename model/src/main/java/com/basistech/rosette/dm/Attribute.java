01c608cad47e25617b4d92bba6a60b29db950d2b
Benson Margulies <benson@basistech.com>
2016-01-29 12:42:33 -0500

    ROS-205: fix apparently harmless mistake in generic decl for Attribute.

--- model/src/main/java/com/basistech/rosette/dm/Attribute.java
+++ model/src/main/java/com/basistech/rosette/dm/Attribute.java
@@ -93,7 +93,7 @@ public abstract class Attribute extends BaseAttribute {
     /**
      * Base class for builders for attributes that inherit from {@link com.basistech.rosette.dm.Attribute}.
      */
-    public abstract static class Builder<T extends BaseAttribute, B extends Builder<T, B>> extends BaseAttribute.Builder<Attribute, Builder<T, B>> {
+    public abstract static class Builder<T extends Attribute, B extends Builder<T, B>> extends BaseAttribute.Builder<Attribute, Builder<T, B>> {
         protected int startOffset;
         protected int endOffset;
 
