d8b8eda31bb6faf68b9363df5445b545edd1174b
Benson Margulies <benson@basistech.com>
2016-02-02 15:55:09 -0500

    ROS-194: remove things that don't make sense on github.

--- json-benchmark/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-time java -cp target/classes:$(cat target/classpath.txt) com.basistech.adm.tools.AdmJsonBenchmark $1
-time java -cp target/classes:$(cat target/classpath.txt) com.basistech.adm.tools.AdmJsonBenchmark $1 -afterburner
-time java -cp target/classes:$(cat target/classpath.txt) com.basistech.adm.tools.AdmJsonBenchmark $1 -smile
-time java -cp target/classes:$(cat target/classpath.txt) com.basistech.adm.tools.AdmJsonBenchmark $1 -smile -afterburner
-
