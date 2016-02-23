62749b52b68d0d45dc3e5828d52401385a95d0be
Benson Margulies <benson@basistech.com>
2016-02-23 14:55:18 -0500

    ROS-43: a bit more testing.

--- json/src/test/java/com/basistech/rosette/dm/json/plain/JsonTest.java
+++ json/src/test/java/com/basistech/rosette/dm/json/plain/JsonTest.java
@@ -20,11 +20,13 @@ import com.basistech.rosette.dm.ArabicMorphoAnalysis;
 import com.basistech.rosette.dm.BaseAttribute;
 import com.basistech.rosette.dm.BaseNounPhrase;
 import com.basistech.rosette.dm.CategorizerResult;
+import com.basistech.rosette.dm.Entity;
 import com.basistech.rosette.dm.Extent;
 import com.basistech.rosette.dm.HanMorphoAnalysis;
 import com.basistech.rosette.dm.KoreanMorphoAnalysis;
 import com.basistech.rosette.dm.LanguageDetection;
 import com.basistech.rosette.dm.ListAttribute;
+import com.basistech.rosette.dm.Mention;
 import com.basistech.rosette.dm.MorphoAnalysis;
 import com.basistech.rosette.dm.RelationshipComponent;
 import com.basistech.rosette.dm.RelationshipMention;
@@ -81,10 +83,12 @@ public class JsonTest extends AdmAssert {
     private TranslatedTokens spanishTranslation;
     private CategorizerResult categoryResult;
     private CategorizerResult sentimentResult;
+    private AnnotatedText referenceTextOldEntities;
     private AnnotatedText referenceText;
+    private Entity entity;
 
     @Before
-    public void oneWithEverything() {
+    public void oneWithEverythingOldEntities() {
         AnnotatedText.Builder builder = new AnnotatedText.Builder();
         builder.data(THIS_IS_THE_TERRIER_SHOT_TO_BOSTON);
         /* Zen text: make me one with everything. */
@@ -276,6 +280,197 @@ public class JsonTest extends AdmAssert {
         crBuilder.add(sentimentResult);
         builder.sentimentResults(crBuilder.build());
 
+        referenceTextOldEntities = builder.build();
+    }
+
+    @Before
+    public void oneWithEverything() {
+        AnnotatedText.Builder builder = new AnnotatedText.Builder();
+        builder.data(THIS_IS_THE_TERRIER_SHOT_TO_BOSTON);
+        /* Zen text: make me one with everything. */
+        ListAttribute.Builder<BaseNounPhrase> bnpListBuilder = new ListAttribute.Builder<>(BaseNounPhrase.class);
+        BaseNounPhrase.Builder bnpBuilder = new BaseNounPhrase.Builder(8, 19);
+        bnpBuilder.extendedProperty("bnp-ex", "bnp-ex-val");
+        baseNounPhrase = bnpBuilder.build();
+        bnpListBuilder.add(baseNounPhrase);
+        builder.baseNounPhrases(bnpListBuilder.build());
+
+        ListAttribute.Builder<Entity> entityListBuilder = new ListAttribute.Builder<>(Entity.class);
+        Entity.Builder entityBuilder = new Entity.Builder();
+        entityBuilder.headMentionIndex(0);
+        entityBuilder.type("PERSON");
+        entityBuilder.entityId("Q100");
+        CategorizerResult.Builder crBuilder = new CategorizerResult.Builder("negative", 0.4);
+        entityBuilder.sentiment(crBuilder.build());
+        Mention.Builder mentionBuilder = new Mention.Builder(0, 10, "PERSON");
+        mentionBuilder.normalized("bahston");
+        mentionBuilder.source("testsource");
+        mentionBuilder.subsource("testsubsource");
+        mentionBuilder.confidence(1.0);
+        mentionBuilder.extendedProperty("em-ex", "em-ex-val");
+        entityBuilder.mention(mentionBuilder.build());
+        entityBuilder.confidence(0.5);
+        entity = entityBuilder.build();
+        entityListBuilder.add(entity);
+        builder.entities(entityListBuilder.build());
+
+        // Build two relation arguments
+        RelationshipComponent.Builder raBuilder = new RelationshipComponent.Builder();
+        raBuilder.phrase("bla");
+        raBuilder.identifier("/free/base/1");
+        raBuilder.extents(Lists.newArrayList(new Extent.Builder(0, 4).build()));
+        RelationshipComponent arg1 = raBuilder.build();
+
+        raBuilder = new RelationshipComponent.Builder();
+        raBuilder.phrase("blu");
+        raBuilder.identifier("/free/base/2");
+        RelationshipComponent arg2 = raBuilder.build();
+
+        raBuilder = new RelationshipComponent.Builder();
+        raBuilder.phrase("bli");
+        raBuilder.identifier("/free/base/3");
+        raBuilder.extents(Lists.newArrayList(new Extent.Builder(5, 6).build(), new Extent.Builder(6, 7).build()));
+        RelationshipComponent pred = raBuilder.build();
+
+        // Build a relation
+        ListAttribute.Builder<RelationshipMention> rmListBuilder = new ListAttribute.Builder<>(RelationshipMention.class);
+        RelationshipMention.Builder rmBuilder = new RelationshipMention.Builder(0, 12).predicate(pred).arg1(arg1).arg2
+                (arg2);
+        rmBuilder.extendedProperty("rm-ex", "rm-ex-val");
+        rmBuilder.source("statistical rules:42");
+        rmBuilder.confidence(1.0);
+        relationshipMention = rmBuilder.build();
+        rmListBuilder.add(relationshipMention);
+        builder.relationshipMentions(rmListBuilder.build());
+
+        ListAttribute.Builder<LanguageDetection> ldListBuilder = new ListAttribute.Builder<>(LanguageDetection.class);
+        List<LanguageDetection.DetectionResult> dets = Lists.newArrayList();
+        dets.add(new LanguageDetection.DetectionResult.Builder(LanguageCode.ENGLISH).encoding("utf-8").script(ISO15924.Latn).confidence(1.0).build());
+        LanguageDetection.Builder ldBuilder = new LanguageDetection.Builder(0, builder.data().length(), dets);
+        ldBuilder.extendedProperty("ld-ex", "ld-ex-val");
+        languageDetectionRegion = ldBuilder.build();
+        ldListBuilder.add(languageDetectionRegion);
+        builder.languageDetectionRegions(ldListBuilder.build());
+
+        dets = Lists.newArrayList();
+        dets.add(new LanguageDetection.DetectionResult.Builder(LanguageCode.FRENCH).encoding("utf-8").script(ISO15924.Latn).confidence(1.0).build());
+        ldBuilder = new LanguageDetection.Builder(0, builder.data().length(), dets);
+        ldBuilder.extendedProperty("ldw-ex", "ldw-ex-val");
+        languageDetection = ldBuilder.build();
+        builder.wholeDocumentLanguageDetection(ldBuilder.build());
+
+        ListAttribute.Builder<ScriptRegion> srListBuilder = new ListAttribute.Builder<>(ScriptRegion.class);
+        ScriptRegion.Builder srBuilder = new ScriptRegion.Builder(0, builder.data().length(), ISO15924.Latn);
+        srBuilder.extendedProperty("sr-ex", "sr-ex-val");
+        scriptRegion = srBuilder.build();
+        srListBuilder.add(scriptRegion);
+        builder.scriptRegions(srListBuilder.build());
+
+        ListAttribute.Builder<Sentence> sentListBuilder = new ListAttribute.Builder<>(Sentence.class);
+        Sentence.Builder sentBuilder = new Sentence.Builder(0, 8);
+        sentBuilder.extendedProperty("sb-ex", "sb-ex-val");
+        sentence = sentBuilder.build();
+        sentListBuilder.add(sentence);
+        builder.sentences(sentListBuilder.build());
+
+        ListAttribute.Builder<Token> tokenListBuilder = new ListAttribute.Builder<>(Token.class);
+        Token.Builder tokenBuilder = new Token.Builder(0, 4, "This");
+        tokenBuilder.source("test");
+        tokenBuilder.addNormalized("abnormal");
+        tokenBuilder.extendedProperty("tok-ex", "tok-ex-val");
+
+        MorphoAnalysis.Builder maBuilder = new MorphoAnalysis.Builder();
+        maBuilder.raw("cooked");
+        maBuilder.partOfSpeech("+woof");
+        Token.Builder compTokBuilder = new Token.Builder(0, 2, "Th");
+        maBuilder.addComponent(compTokBuilder.build());
+        morphoAnalysis = maBuilder.build();
+        tokenBuilder.addAnalysis(morphoAnalysis);
+
+        ArabicMorphoAnalysis.Builder araMaBuilder = new ArabicMorphoAnalysis.Builder();
+        araMaBuilder.addPrefix("pre", "PRE");
+        araMaBuilder.addStem("stem", "STEM");
+        araMaBuilder.addSuffix("suff", "SUFF");
+        araMaBuilder.definiteArticle(true);
+        araMaBuilder.strippablePrefix(true);
+        araMaBuilder.root("root");
+        araMaBuilder.lengths(2, 3);
+        araMaBuilder.lemma("lemma");
+        araMaBuilder.partOfSpeech("pos");
+        araMaBuilder.raw("raw");
+        tokenBuilder.addAnalysis(araMaBuilder.build());
+
+        HanMorphoAnalysis.Builder hanMaBuilder = new HanMorphoAnalysis.Builder();
+        hanMaBuilder.addReading("proust");
+        hanMaBuilder.lemma("lemma");
+        hanMaBuilder.partOfSpeech("pos");
+        tokenBuilder.addAnalysis(hanMaBuilder.build());
+
+        KoreanMorphoAnalysis.Builder korMaBuilder = new KoreanMorphoAnalysis.Builder();
+        korMaBuilder.addMorpheme("m1", "t1");
+        korMaBuilder.addMorpheme("m2", "t2");
+        korMaBuilder.partOfSpeech("korean");
+        korMaBuilder.lemma("koreanLemma");
+        tokenBuilder.addAnalysis(korMaBuilder.build());
+
+        token = tokenBuilder.build();
+        tokenListBuilder.add(token);
+        builder.tokens(tokenListBuilder.build());
+
+        ListAttribute.Builder<TranslatedData> translatedDataBuilder =
+                new ListAttribute.Builder<>(TranslatedData.class);
+
+        germanDomain = new TextDomain(ISO15924.Latn, LanguageCode.GERMAN, TransliterationScheme.NATIVE);
+        String germanText = "Ein.  Zwei.";
+        TranslatedData.Builder tdBuilder = new TranslatedData.Builder(germanDomain, germanText);
+        germanTranslatedData = tdBuilder.build();
+        translatedDataBuilder.add(germanTranslatedData);
+        spanishDomain = new TextDomain(ISO15924.Latn, LanguageCode.SPANISH, TransliterationScheme.NATIVE);
+        String spanishText = "Uno.  Dos.";
+        tdBuilder = new TranslatedData.Builder(spanishDomain, spanishText);
+        spanishTranslatedData = tdBuilder.build();
+        translatedDataBuilder.add(spanishTranslatedData);
+        builder.translatedData(translatedDataBuilder.build());
+
+        ListAttribute.Builder<TranslatedTokens> translatedTokensListBuilder =
+                new ListAttribute.Builder<>(TranslatedTokens.class);
+
+        TranslatedTokens.Builder ttBuilder = new TranslatedTokens.Builder(germanDomain);
+        ttBuilder.addTranslatedToken("Ein");
+        ttBuilder.addTranslatedToken(".");
+        ttBuilder.addTranslatedToken("Zwei");
+        ttBuilder.addTranslatedToken(".");
+        germanTranslation = ttBuilder.build();
+        translatedTokensListBuilder.add(germanTranslation);
+        spanishDomain = new TextDomain(ISO15924.Latn, LanguageCode.SPANISH, TransliterationScheme.NATIVE);
+        ttBuilder = new TranslatedTokens.Builder(spanishDomain);
+        ttBuilder.addTranslatedToken("Uno");
+        ttBuilder.addTranslatedToken(".");
+        ttBuilder.addTranslatedToken("Dos");
+        ttBuilder.addTranslatedToken(".");
+        spanishTranslation = ttBuilder.build();
+        translatedTokensListBuilder.add(spanishTranslation);
+        builder.translatedTokens(translatedTokensListBuilder.build());
+
+        ListAttribute.Builder<CategorizerResult> crListBuilder = new ListAttribute.Builder<>(CategorizerResult.class);
+        Map<String, Double> perFeatureScores = Maps.newHashMap();
+        perFeatureScores.put("foo", 1.2);
+        perFeatureScores.put("bar", -2.4);
+        categoryResult = new CategorizerResult.Builder("POLITICS", -0.2)
+                .confidence(0.3)
+                .explanationSet(Lists.newArrayList("foo", "bar"))
+                .perFeatureScores(perFeatureScores).build();
+        crListBuilder.add(categoryResult);
+        builder.categorizerResults(crListBuilder.build());
+
+        crListBuilder = new ListAttribute.Builder<>(CategorizerResult.class);
+        sentimentResult = new CategorizerResult.Builder("negative", -0.2)
+                .confidence(0.3)
+                .explanationSet(Lists.newArrayList("foo", "bar"))
+                .perFeatureScores(perFeatureScores).build();
+        crListBuilder.add(sentimentResult);
+        builder.sentimentResults(crListBuilder.build());
+
         referenceText = builder.build();
     }
 
@@ -326,7 +521,7 @@ public class JsonTest extends AdmAssert {
         StringWriter writer = new StringWriter();
         ObjectMapper mapper = AnnotatedDataModelModule.setupObjectMapper(new ObjectMapper());
         ObjectWriter objectWriter = mapper.writer();
-        objectWriter.writeValue(writer, referenceText);
+        objectWriter.writeValue(writer, referenceTextOldEntities);
         // to tell that the version is there, we read as a tree
         JsonNode tree = mapper.readTree(writer.toString());
         assertEquals("1.1.0", tree.get("version").asText());
@@ -337,7 +532,7 @@ public class JsonTest extends AdmAssert {
         StringWriter writer = new StringWriter();
         ObjectMapper mapper = AnnotatedDataModelModule.setupObjectMapper(new ObjectMapper());
         ObjectWriter objectWriter = mapper.writer();
-        objectWriter.writeValue(writer, referenceText);
+        objectWriter.writeValue(writer, referenceTextOldEntities);
         mapper.readValue(writer.toString(), AnnotatedText.class);
     }
 
@@ -355,11 +550,11 @@ public class JsonTest extends AdmAssert {
     }
 
     @Test
-    public void roundTrip() throws Exception {
+    public void roundTripOldEntities() throws Exception {
         StringWriter writer = new StringWriter();
         ObjectMapper mapper = AnnotatedDataModelModule.setupObjectMapper(new ObjectMapper());
         ObjectWriter objectWriter = mapper.writer();
-        objectWriter.writeValue(writer, referenceText);
+        objectWriter.writeValue(writer, referenceTextOldEntities);
 
         ObjectReader reader = mapper.readerFor(AnnotatedText.class);
         AnnotatedText read = reader.readValue(writer.toString());
@@ -425,6 +620,70 @@ public class JsonTest extends AdmAssert {
     }
 
     @Test
+    public void roundTrip() throws Exception {
+        StringWriter writer = new StringWriter();
+        ObjectMapper mapper = AnnotatedDataModelModule.setupObjectMapper(new ObjectMapper());
+        ObjectWriter objectWriter = mapper.writer();
+        objectWriter.writeValue(writer, referenceText);
+
+        ObjectReader reader = mapper.readerFor(AnnotatedText.class);
+        AnnotatedText read = reader.readValue(writer.toString());
+
+        ListAttribute<BaseNounPhrase> bnpList = read.getBaseNounPhrases();
+        assertNotNull(bnpList);
+        assertEquals(1, bnpList.size());
+        BaseNounPhrase bnp = bnpList.get(0);
+        assertEquals(baseNounPhrase, bnp);
+
+        ListAttribute<Entity> entityList = read.getEntities();
+        assertNotNull(entityList);
+        assertEquals(1, entityList.size());
+        Entity en = entityList.get(0);
+        assertEquals(entity, en);
+
+        ListAttribute<RelationshipMention> rmList = read.getRelationshipMentions();
+        assertNotNull(rmList);
+        assertEquals(1, rmList.size());
+        RelationshipMention rm = rmList.get(0);
+        assertEquals(relationshipMention, rm);
+
+        ListAttribute<LanguageDetection> languageDetectionList = read.getLanguageDetectionRegions();
+        assertNotNull(languageDetectionList);
+        assertEquals(1, languageDetectionList.size());
+
+        assertEquals(languageDetectionRegion, languageDetectionList.get(0));
+        assertEquals(languageDetection,  read.getWholeTextLanguageDetection());
+
+        ListAttribute<ScriptRegion> scriptRegionList = read.getScriptRegions();
+        assertNotNull(scriptRegionList);
+        assertEquals(1, scriptRegionList.size());
+
+        assertEquals(scriptRegion, scriptRegionList.get(0));
+
+        ListAttribute<Sentence> sentences = read.getSentences();
+        assertNotNull(sentences);
+
+        assertEquals(sentence, sentences.get(0));
+
+        ListAttribute<Token> tokenList = read.getTokens();
+        assertNotNull(tokenList);
+        assertEquals(1, tokenList.size());
+        assertEquals(token, tokenList.get(0));
+
+        ListAttribute<TranslatedData> dataTranslations = read.getTranslatedData();
+        assertEquals(germanTranslatedData, dataTranslations.get(0));
+        assertEquals(spanishTranslatedData, dataTranslations.get(1));
+
+        ListAttribute<TranslatedTokens> translatedTokens = read.getTranslatedTokens();
+        assertEquals(germanTranslation, translatedTokens.get(0));
+        assertEquals(spanishTranslation, translatedTokens.get(1));
+
+        assertEquals(categoryResult, read.getCategorizerResults().get(0));
+
+        assertEquals(sentimentResult, read.getSentimentResults().get(0));
+    }
+
+    @Test
     public void testForwardCompatibilitySimple() throws Exception {
         ObjectMapper mapper = objectMapper();
         AnnotatedText.Builder builder = new AnnotatedText.Builder();
@@ -450,7 +709,7 @@ public class JsonTest extends AdmAssert {
     @Test
     public void testForwardCompatibilityNoisy() throws Exception {
         ObjectMapper mapper = objectMapper();
-        JsonNode tree = mapper.valueToTree(referenceText);
+        JsonNode tree = mapper.valueToTree(referenceTextOldEntities);
         ObjectNode attributes = (ObjectNode) tree.get("attributes");
         ObjectNode extendedObject = attributes.putObject("novelty");
         extendedObject.put("type", "novelty");
