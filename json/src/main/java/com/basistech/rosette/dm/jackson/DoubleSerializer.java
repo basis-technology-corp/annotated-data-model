17a0ac8265d13c4f890b638aca6868dd08bcaf7a
Ian Redpath <iredpath@basistech.com>
2017-09-21 13:07:48 -0400

    remove unused dependency

--- json/src/main/java/com/basistech/rosette/dm/jackson/DoubleSerializer.java
+++ json/src/main/java/com/basistech/rosette/dm/jackson/DoubleSerializer.java
@@ -18,7 +18,6 @@
 package com.basistech.rosette.dm.jackson;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
